a8c3b77ad34e793d1b0fe2e831d0f4a609bfe724
Selasi3 <selasiayittah3@gmail.com>
2021-10-17 03:12:39 -1200

    new_changes

--- src/main/kotlin/basic/Exceptions.kt
+++ src/main/kotlin/basic/Exceptions.kt
@@ -1,14 +1,11 @@
-//fun getAge(name: String) : int? =
-//    if(name in people) people[name] else
-//        throw PersonNotFoundException("$name not found.")
+//fun getAge(name:String):Int? = if(name in people) people[name] else throw PersonNotFoundException("$name not found.")
 //
-//object people {
 //
-//}
 //
-//class PersonNotFoundException (message:String) :
-//        Throwable(message)
+//
+//class PersonNotFoundException(message: String?) : Throwable(message)
 //
 //fun main(){
-//    getAge("Charle")
+//    getAge("Charlie")
+//
 //}
--- src/main/kotlin/classes/OnlyPositiveInts.kt
+++ src/main/kotlin/classes/OnlyPositiveInts.kt
@@ -2,6 +2,7 @@ package classes
 
 class OnlyPositiveInts(n:Int) {
     var number :Int =0
+//        providing our own setter
     set(value) {field = if (value<0) 0 else value}
 
     init {
